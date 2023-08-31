c61d9b3c9e94f64c3ceb0d2e4b1bfafe7bf91949
mikanbox <19262239+mikanbox@users.noreply.github.com>
2023-08-31 22:36:17 +0900

    [add] add simple test code

--- test/nine_scaling_image_test.dart
+++ test/nine_scaling_image_test.dart
@@ -1,10 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nine_scaling_image/nine_scaling_image.dart';
 
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
-  });
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('test the thing', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MediaQuery(
+        data: MediaQueryData(size: Size(200, 300)),
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: NineScalingImage(assetPath: 'test/sampleimage.png'),
+        ),
+      ),
+    );
+
+    expect(
+      find.byType(NineScalingImage), findsOneWidget
+    );
+
+  } );
 }
